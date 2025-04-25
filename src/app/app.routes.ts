e31c25ad6cbb2917776db482b635b7626a95c604
Henry Keniding Tarazona Lazaro <115328041+Keniding@users.noreply.github.com>
2025-04-25 18:21:16 -0500

    feat: host-shell federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {loadRemoteModule} from '@angular-architects/native-federation';
+import {NotFoundComponent} from './page/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent,
+  //   pathMatch: 'full',
+  // },
+
+  // {
+  //   path: '',
+  //   loadComponent: () =>
+  //     loadRemoteModule('mfe-header', './Component')
+  //       .then((module) => module.component),
+  // },
+
+  {
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule('mfe-header', './Component')
+        .then(m => m.AppComponent),
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
+];
